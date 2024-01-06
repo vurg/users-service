f6bd742e8b8f64e23fbcdb1018833adda750cb77
jessie <luhui930@126.com>
2024-01-06 01:18:21 +0100

    fixbug for cors

--- UserService/settings.py
+++ UserService/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['host.docker.internal', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['host.docker.internal', 'localhost', '127.0.0.1','*']
 
 APPEND_SLASH = True
 
